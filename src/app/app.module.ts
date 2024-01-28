f0a55553c729d4257902fdf57962bf9035445bc8
alxxalex <105727854+alxxalex@users.noreply.github.com>
2024-01-28 09:51:44 +0200

    Upload-video

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UploadVideoComponent } from './components/upload-video/upload-video.component';
+import { FormsModule } from '@angular/forms';
+import { NgxFileDropModule } from 'ngx-file-drop';
+import { HttpClientModule } from "@angular/common/http";
+import { HeaderComponent } from './components/header/header.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadVideoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NgxFileDropModule,
+    // MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
